64775139cdab2bcc82448559b54e57e79686e0f0
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-08-13 04:05:48 +0000

    Added in the classes I left out of the last commit.

---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/Cost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop
+{
+	//		Cost
+	//			Base Cost
+	//			Currency Type
+	public class Cost
+	{
+		public int weaponCost;
+		public string currencyType;
+
+		public Cost(int cost, string currency)
+		{
+			weaponCost = cost;
+			currencyType = currency;
+		}
+
+		public string DisplayWeaponCost()
+		{
+			return String.Format("Weapon Cost:\t{0} {1}", weaponCost.ToString(), currencyType);
+		}
+
+		public override string ToString()
+		{
+			return String.Format("Weapon Cost:\t{0} {1}", weaponCost.ToString(), currencyType);
+		}
+		// Operate on the cost
+		// Display the cost and currency
+	}
+}
---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/PreRequisites.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop
+{
+	//		Pre-requisites
+	//			Proficiency
+	//			Category
+	//			Attack Type
+	public class PreRequisites
+	{
+		public string weaponProficiency;
+		public string weaponCategory;
+		public string attackType;
+
+		public PreRequisites(string proficiency, string category, string type)
+		{
+			weaponProficiency = proficiency;
+			weaponCategory = category;
+			attackType = type;
+		}
+
+		public string DisplayPreRequisites()
+		{
+			return String.Format("Weapon Proficiency:\t{0}\rWeapon Category:\t{1}\rWeapon Attack Type:\t{2}", weaponProficiency, weaponCategory, attackType);
+		}
+
+		public override string ToString()
+		{
+			return String.Format("Weapon Proficiency:\t{0}\rWeapon Category:\t{1}\rWeapon Attack Type:\t{2}", weaponProficiency, weaponCategory, attackType);
+		}
+		// Display the proficiency
+	}
+}
