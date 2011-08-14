60d4031d21d5e7e49747f4b1bbcad6ba88a8570f
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-08-14 18:41:52 +0000

    More classes for composition pattern

---
+++ ItemSmithWorkShop/ItemSmithWorkShop/DisplayWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop
+{
+	public class DisplayWeapon
+	{
+		public string WeaponName { get; set; }
+		public string WeaponPreRequisites { get; set; }
+		public string WeaponCost { get; set; }
+		public string WeaponDamage { get; set; }
+
+		public DisplayWeapon(Weapon weapon, PreRequisites preRequisites, Cost cost)
+		{
+			WeaponName = weapon.ToString();
+			WeaponPreRequisites = preRequisites.ToString();
+			WeaponCost = cost.ToString();
+		}
+
+		public DisplayWeapon(Weapon weapon, PreRequisites preRequisites, Cost cost, Damage damage)
+		{
+			WeaponName = weapon.ToString();
+			WeaponPreRequisites = preRequisites.ToString();
+			WeaponCost = cost.ToString();
+			WeaponDamage = damage.ToString();
+		}
+
+		public override string ToString()
+		{
+			return String.Format("{0}\r{1}\r{2}\r{3}", WeaponName, WeaponPreRequisites, WeaponCost, WeaponDamage);
+		}
+	}
+}
---
+++ ItemSmithWorkShop/ItemSmithWorkShop/Weight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop
+{
+	//		Weight
+	//			Hardness
+	//			Hit Points
+	public class Weight
+	{
+		public int hardness;
+		public int hitPoints;
+
+		public Weight(int durability, int hits)
+		{
+			hardness = durability;
+			hitPoints = hits;
+		}
+	}
+}
