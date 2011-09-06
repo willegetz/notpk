c1b4c055615447d8c7213f75abbb505b3b683427
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-09-06 07:16:46 +0000

    I have a dagger. I have dagger special materials. I have dagger sizes. Next: Magic and refactoring.

--- ItemSmithWorkShop/ItemSmithWeaponSmith/SimpleDagger.cs
+++ ItemSmithWorkShop/ItemSmithWeaponSmith/SimpleDagger.cs
@@ -10,8 +10,6 @@ namespace ItemSmithWeaponSmith
 		private double masterworkCostModifier;
 		public int BasePrice { get; set; }
 
-		//public string WeaponName { get; set; }
-
 		public string WeaponProficiencyRequirement
 		{
 			get
@@ -27,12 +25,6 @@ namespace ItemSmithWeaponSmith
 				return "Melee";
 			}
 		}
-
-		//public string WeaponDamage
-		//{
-		//    get;
-		//    set;
-		//}
 
 		public string WeaponThreatRange
 		{
@@ -64,24 +56,6 @@ namespace ItemSmithWeaponSmith
 			set;
 		}
 
-		//public double WeaponWeight
-		//{
-		//    get;
-		//    set;
-		//}
-
-		//public double WeaponHardness
-		//{
-		//    get;
-		//    set;
-		//}
-
-		//public double WeaponHitPoints
-		//{
-		//    get;
-		//    set;
-		//}
-
 		public string WeaponText { get; set; }
 		public bool IsMasterwork { get; private set; }
 		public int ToHitModifier { get; set; }
@@ -104,82 +78,6 @@ namespace ItemSmithWeaponSmith
 			masterworkCostModifier = 300;
 		}
 
-		//private void DetermineWeaponDamage(string weaponSize)
-		//{
-		//    Dictionary<string, string> damageAdjustment = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-		//    var sb = new StringBuilder();
-
-		//    damageAdjustment.Add("Fine", "No Meaningful Damage");
-		//    damageAdjustment.Add("Diminutive", "1");
-		//    damageAdjustment.Add("Tiny", "1d2");
-		//    damageAdjustment.Add("Small", "1d3");
-		//    damageAdjustment.Add("Medium", "1d4");
-		//    damageAdjustment.Add("Large", "1d6");
-		//    damageAdjustment.Add("Huge", "1d8");
-		//    damageAdjustment.Add("Gargantuan", "2d6");
-		//    damageAdjustment.Add("Colossal", "3d6");
-
-		//    if (damageAdjustment.ContainsKey(weaponSize))
-		//    {
-		//        WeaponDamage = damageAdjustment[weaponSize];
-		//        WeaponName = String.Format("{0} ({1})", WeaponName, weaponSize);
-		//    }
-		//}
-
-		//public void AdjustHardnessHitPointsForSize(string weaponSize)
-		//{
-		//    Dictionary<string, double> hardnessHitPointsAdjustment = new Dictionary<string, double>(StringComparer.OrdinalIgnoreCase);
-
-		//    hardnessHitPointsAdjustment.Add("Fine", 0.0625);
-		//    hardnessHitPointsAdjustment.Add("Diminutive", 0.125);
-		//    hardnessHitPointsAdjustment.Add("Tiny", 0.25);
-		//    hardnessHitPointsAdjustment.Add("Small", 0.5);
-		//    hardnessHitPointsAdjustment.Add("Medium", 1);
-		//    hardnessHitPointsAdjustment.Add("Large", 2);
-		//    hardnessHitPointsAdjustment.Add("Huge", 4);
-		//    hardnessHitPointsAdjustment.Add("Gargantuan", 8);
-		//    hardnessHitPointsAdjustment.Add("Colossal", 16);
-
-		//    if (hardnessHitPointsAdjustment.ContainsKey(weaponSize))
-		//    {
-		//        WeaponHardness = (WeaponHardness * hardnessHitPointsAdjustment[weaponSize]);
-		//        WeaponHitPoints = (WeaponHitPoints * hardnessHitPointsAdjustment[weaponSize]);
-		//    }
-		//    else
-		//    {
-		//        WeaponHardness = 10;
-		//        WeaponHitPoints = 2;
-		//    }
-		//    if (WeaponHardness < 1)
-		//    {
-		//        WeaponHardness = 1;
-		//    }
-		//    if (WeaponHitPoints < 1)
-		//    {
-		//        WeaponHitPoints = 1;
-		//    }
-		//}
-
-		//public void AdjustWeightForSize(string weaponSize)
-		//{
-		//    Dictionary<string, double> weightAdjustment = new Dictionary<string, double>(StringComparer.OrdinalIgnoreCase);
-
-		//    weightAdjustment.Add("Small", 0.5);
-		//    weightAdjustment.Add("Large", 2);
-
-		//    if (weightAdjustment.ContainsKey(weaponSize))
-		//    {
-		//        WeaponWeight *= weightAdjustment[weaponSize];
-		//    }
-		//}
-
-		//public void AdjsutCostForSize(string weaponSize)
-		//{
-		//    Dictionary<string, double> costAdjustment = new Dictionary<string, double>(StringComparer.OrdinalIgnoreCase);
-
-		//    costAdjustment.Add("Large", 2);
-		//}
-
 		public SimpleDagger(string weaponSize)
 		{
 			WeaponName = "Dagger";
@@ -192,21 +90,6 @@ namespace ItemSmithWeaponSmith
 			CheckForNull(weaponSize);
 			WeaponCost = BasePrice;
 		}
-
-		//private void CheckForNull(string weaponSize)
-		//{
-		//    if (weaponSize == null)
-		//    {
-		//        return;
-		//    }
-		//    else
-		//    {
-		//        DetermineWeaponDamage(weaponSize);
-		//        AdjustHardnessHitPointsForSize(weaponSize);
-		//        AdjustWeightForSize(weaponSize);
-		//        AdjsutCostForSize(weaponSize);
-		//    }
-		//}
 
 		private string WeaponToDisplay()
 		{
