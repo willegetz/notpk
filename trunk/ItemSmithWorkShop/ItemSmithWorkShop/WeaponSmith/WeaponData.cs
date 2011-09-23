e312f61481d7032ce90c7bc1e7a5d72b676ef254
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-09-23 20:48:10 +0000

    More Extraction

--- trunk/ItemSmithWorkShop/ItemSmithWorkShop/WeaponSmith/WeaponData.cs
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/WeaponSmith/WeaponData.cs
@@ -7,17 +7,17 @@ namespace ItemSmithWorkShop
 {
 	public class WeaponData
 	{
-		public string WeaponName = "Dagger";
-		public string WeaponCategory = "Melee";
-		public string WeaponProficiencyRequirement = "Light Weapon";
-		public string WeaponDamage = "1d4";
-		public string WeaponThreatRange = "19-20";
-		public string WeaponCritical = "x2";
-		public string WeaponDamageType = "Piercing or Slashing";
-		public double WeaponHardness = 10;
-		public double WeaponHitPoints = 2;
-		public double BasePrice = 2;
-		public double WeaponWeight = 1;
-		public string WeaponText = "The dagger is a common secondary weapon. You get a +2 bonus on\n\tSleight of Hand checks made to conceal a dagger on your body.";
+		public string WeaponName { get; set; }
+		public string WeaponCategory { get; set; }
+		public string WeaponProficiencyRequirement { get; set; }
+		public string WeaponDamage { get; set; }
+		public string WeaponThreatRange { get; set; }
+		public string WeaponCritical { get; set; }
+		public string WeaponDamageType { get; set; }
+		public double WeaponHardness { get; set; }
+		public double WeaponHitPoints { get; set; }
+		public double BasePrice { get; set; }
+		public double WeaponWeight { get; set; }
+		public string WeaponText { get; set; }
 	}
 }
