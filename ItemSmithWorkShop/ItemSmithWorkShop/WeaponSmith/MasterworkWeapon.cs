f1a76bb2423b5fe94f18856a9aff1158f7d3f41c
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-10-19 06:29:29 +0000

    Passing tests on the MasterworkWeapon.cs class for both single and double weapons.

--- ItemSmithWorkShop/ItemSmithWorkShop/WeaponSmith/MasterworkWeapon.cs
+++ ItemSmithWorkShop/ItemSmithWorkShop/WeaponSmith/MasterworkWeapon.cs
@@ -7,20 +7,38 @@ namespace ItemSmithWorkShop.WeaponSmith
 {
 	public class MasterworkWeapon
 	{
+		private static List<GenericWeapon> weaponHeadList = new List<GenericWeapon>();
 		private static double singleWeaponMasterworkCost = 300;
 
-		internal static void MakeMasterwork(GenericWeapon testDagger)
+		internal static void MakeMasterwork(GenericWeapon singleWeapon)
 		{
-			SetMasterworkProperties(testDagger);
+			SetMasterworkProperties(singleWeapon);
 		}
 
-		private static void SetMasterworkProperties(GenericWeapon testDagger)
+		internal static void MakeMasterwork(GenericDoubleWeapon doubleWeapon)
 		{
-			testDagger.IsMasterwork = true;
-			testDagger.MasterWorkLabel = " [Masterwork]";
-			testDagger.ToHitModifier = 1;
-			testDagger.WeaponText = testDagger.WeaponText + string.Format("\n\tThis {0} is masterwork quality!", testDagger.WeaponType.ToLower());
-			testDagger.MasterworkCost = singleWeaponMasterworkCost;
+			weaponHeadList.Add(doubleWeapon.FirstWeaponPart);
+			weaponHeadList.Add(doubleWeapon.SecondWeaponPart);
+
+			SetMasterworkProperties(doubleWeapon);
+		}
+
+		private static void SetMasterworkProperties(GenericDoubleWeapon doubleWeapon)
+		{
+			foreach (var weaponHead in weaponHeadList)
+			{
+				SetMasterworkProperties(weaponHead);
+			}
+		}
+
+		private static void SetMasterworkProperties(GenericWeapon singleWeapon)
+		{
+			singleWeapon.IsMasterwork = true;
+			singleWeapon.MasterWorkLabel = " [Masterwork]";
+			singleWeapon.ToHitModifier = 1;
+			singleWeapon.WeaponText = singleWeapon.WeaponText + string.Format("\n\tThis {0} is masterwork quality!", singleWeapon.WeaponType.ToLower());
+			CalculateWeaponCosts.SetMasterworkCost(singleWeaponMasterworkCost);
+			singleWeapon.TotalWeaponCost += CalculateWeaponCosts.GetMasterworkCost();
 		}
 	}
 }
