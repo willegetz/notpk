388ed9f6f87fb66c780da15bc33f17cb8afc8c74
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-10-22 00:47:51 +0000

    Displaying basic single and double weapons

--- trunk/ItemSmithWorkShop/ItemSmithWorkShop/Tests/WeaponSmithTests/NewCreationFlowTests.cs
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/Tests/WeaponSmithTests/NewCreationFlowTests.cs
@@ -3,6 +3,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests;
+using ItemSmithWorkShop.WeaponSmith;
 
 namespace ItemSmithWorkShop.Tests.WeaponSmithTests
 {
@@ -10,8 +12,24 @@ namespace ItemSmithWorkShop.Tests.WeaponSmithTests
 	public class NewCreationFlowTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void TestCreateWeaponClass()
 		{
+			var weaponData = TempWeaponDictionary.GetWeaponData("Dagger");
+			
+			var dagger = new CreateWeapon(weaponData);
+
+			Approvals.Approve(dagger.DisplayWeapon());
+		}
+
+		[TestMethod]
+		public void TestCreateDoubleWeaponWithNewClass()
+		{
+			var weaponData1 = TempWeaponDictionary.GetWeaponData("Dwarven Urgrosh A");
+			var weaponData2 = TempWeaponDictionary.GetWeaponData("Dwarven Urgrosh B");
+
+			var weapon = new CreateWeapon(weaponData1, weaponData2);
+
+			Approvals.Approve(weapon.DisplayWeapon());
 		}
 	}
 }
