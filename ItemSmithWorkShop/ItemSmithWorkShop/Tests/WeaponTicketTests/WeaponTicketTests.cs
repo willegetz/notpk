070baae7c3a4cd5e1389f371ce4c14b1ea9d4407
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-10-27 13:23:32 +0000

    Begin with weaponticket.

---
+++ ItemSmithWorkShop/ItemSmithWorkShop/Tests/WeaponTicketTests/WeaponTicketTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests;
+using ItemSmithWorkShop.WeaponTicket;
+
+namespace ItemSmithWorkShop.Tests.WeaponTicketTests
+{
+	[TestClass]
+	public class WeaponTicketTests
+	{
+		[TestMethod]
+		public void TestWeaponTicket()
+		{
+			var ticket = new WeaponTicket();
+
+			ticket.AddLineItem(new WeaponHead("Dagger"));
+
+			Approvals.Approve(ticket);
+		}
+	}
+}
