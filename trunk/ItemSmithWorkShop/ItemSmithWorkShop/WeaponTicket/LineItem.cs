be7be07136ec27d6fa6265d1a70c534ec1bf11e6
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-10-27 13:23:32 +0000

    Begin with weaponticket.

---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/WeaponTicket/LineItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop.WeaponTicket
+{
+	class LineItem
+	{
+		public virtual string WeaponHeadName()
+		{
+			return string.Empty;
+		}
+
+		public virtual string Category()
+		{
+			return string.Empty;
+		}
+
+		public virtual string ProficiencyRequirement()
+		{
+			return string.Empty;
+		}
+
+		public virtual string Damage()
+		{
+			return string.Empty;
+		}
+	}
+}
---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/WeaponTicket/WeaponTicket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop.WeaponTicket
+{
+	class WeaponTicket
+	{
+		internal void AddLineItem(WeaponHead weaponHead)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
