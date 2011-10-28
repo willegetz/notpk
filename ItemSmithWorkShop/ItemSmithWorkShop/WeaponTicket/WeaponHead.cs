501d1135397a4487ed5cbb5fa3f7894be43e7a00
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-10-28 00:04:39 +0000

    Trying to display line items in a column format. Not working well.

--- ItemSmithWorkShop/ItemSmithWorkShop/WeaponTicket/WeaponHead.cs
+++ ItemSmithWorkShop/ItemSmithWorkShop/WeaponTicket/WeaponHead.cs
@@ -2,10 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ItemSmithWorkShop.WeaponSmith;
 
-namespace ItemSmithWorkShop.WeaponTicket
+namespace ItemSmithWorkShop.WeaponOrder
 {
 	class WeaponHead : LineItem
 	{
+		WeaponData weaponData;
+		List<WeaponData> data;
+
+		public WeaponHead(string name)
+		{
+			weaponData = TempWeaponDictionary.GetWeaponData(name);
+		}
+
+		public override string WeaponHeadName()
+		{
+			return weaponData.WeaponName;
+		}
+
+		public override string Category()
+		{
+			return weaponData.WeaponCategory;
+		}
+
+		public override string ProficiencyRequirement()
+		{
+			return weaponData.WeaponProficiencyRequirement;
+		}
+
+		public override string Damage()
+		{
+			return weaponData.WeaponDamage;
+		}
+
+		public override string DamageType()
+		{
+			return weaponData.WeaponDamageType;
+		}
+
+		public override string ThreatRange()
+		{
+			return weaponData.WeaponThreatRange;
+		}
+
+		public override string CriticalMultiplier()
+		{
+			return weaponData.WeaponCritical;
+		}
+
+		public override string ToString()
+		{
+			List<int> stringLength = new List<int>();
+
+			var sb = new StringBuilder();
+
+			sb.Append(string.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}", WeaponHeadName(), Category(), ProficiencyRequirement(), Damage(), DamageType(), ThreatRange(), CriticalMultiplier()));
+			
+			return sb.ToString();
+
+		}
 	}
 }
