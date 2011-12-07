de32cd770a1b7d61e1f0dc2af27f26e9f64649cf
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-12-07 06:56:40 +0000

    Spiked a decorator pattern with ItemSmith.

---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/NewWeaponSmith/AdventureItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewWeaponSmith
+{
+	public abstract class AdventureItem
+	{
+		public string itemName = "Unknown Item";
+
+		public virtual string GetItemName()
+		{
+			return itemName;
+		}
+
+		public abstract double GetCost();
+	}
+}
---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/NewWeaponSmith/AdventureWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewWeaponSmith
+{
+	public class AdventureWeapon : AdventureItem
+	{
+
+		public AdventureWeapon(string weaponName)
+		{
+			itemName = weaponName;
+		}
+
+		public override double GetCost()
+		{
+			return 2;
+		}
+
+	}
+}
