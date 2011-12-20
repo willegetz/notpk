0d6d29e3af546473cfa7a3aed97620534aa7f193
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-12-20 04:09:31 +0000

    Added DisplayUtilities.cs class. Masterwork material now uses DisplayUtilities.

---
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/WeaponUtilities/DisplayUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemSmithWorkShop.WeaponUtilities
+{
+	public class DisplayUtilities
+	{
+		internal static string BasicDisplay(string name, double cost, double weight, string toHit, string damage, string description)
+		{
+			return string.Format("{0}:\t'{1} gp'\r\nWeight: '{2} pound(s)'\r\nTo Hit: '{3}'\r\nDamage: '{4}'\r\n\t{5}", name, cost, weight, toHit, damage, description);
+		}
+	}
+}
