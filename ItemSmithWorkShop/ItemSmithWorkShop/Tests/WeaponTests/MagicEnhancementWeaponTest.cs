a07dfdf4c4392c4a6c278f14085700d425b456c6
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2011-12-24 07:34:43 +0000

    More file reorganization

---
+++ ItemSmithWorkShop/ItemSmithWorkShop/Tests/WeaponTests/MagicEnhancementWeaponTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventureItems;
+using ApprovalTests;
+using ItemSmithWorkShop.AdventureItems.WeaponAdons;
+using ItemSmithWorkShop.AdventureItems.ExtraordinaryWeaponUtilities;
+
+namespace ItemSmithWorkShop.Tests.WeaponTests
+{
+	[TestClass]
+	public class MagicEnhancementWeaponTest
+	{
+		[TestMethod]
+		public void TestCreateMagicWeapon()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(weapon, 1);
+			Approvals.Approve(magicWeapon.GetItem());
+		}
+
+		[TestMethod]
+		public void TestCreateMagicAdamantineWeapon()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			ExtraordinaryQualityWeapon adamantineWeapon = WeaponItemSmith.OrderSpecialComponent(weapon, "Adamantine");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(adamantineWeapon, 1);
+			Approvals.Approve(magicWeapon.GetItem());
+		}
+
+		[TestMethod]
+		public void TestCreateMagicAlchemicalSilverWeapon()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			ExtraordinaryQualityWeapon silverWeapon = WeaponItemSmith.OrderSpecialComponent(weapon, "Silver");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(silverWeapon, 1);
+			Approvals.Approve(magicWeapon.GetItem());
+		}
+
+		[TestMethod]
+		public void TestCreateMagicColdIronWeapon()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			ExtraordinaryQualityWeapon ironWeapon = WeaponItemSmith.OrderSpecialComponent(weapon, "Cold Iron");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(ironWeapon, 1);
+			Approvals.Approve(magicWeapon.GetItem());
+		}
+
+		[TestMethod]
+		public void TestCreateMagicDarkwoodWeapon()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			ExtraordinaryQualityWeapon darkwoodWeapon = WeaponItemSmith.OrderSpecialComponent(weapon, "Darkwood");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(darkwoodWeapon, 1);
+			Approvals.Approve(magicWeapon.GetItem());
+		}
+
+		[TestMethod]
+		public void TestCreateMagicMithralWeapon()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			ExtraordinaryQualityWeapon mithralWeapon = WeaponItemSmith.OrderSpecialComponent(weapon, "Mithral");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(mithralWeapon, 1);
+			Approvals.Approve(magicWeapon.GetItem());
+		}
+
+		[TestMethod]
+		public void TestDisplayFullMagicItemText()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(weapon, 1);
+			Approvals.Approve(magicWeapon.DisplayFullText());
+		}
+
+		[TestMethod]
+		public void TestDisplayMaxEnhancedWeaponItem()
+		{
+			WeaponItem weapon = WeaponItemSmith.OrderItem("Dagger");
+			MagicWeaponItem magicWeapon = new MagicWeaponItem(weapon, 5);
+			Approvals.Approve(magicWeapon.DisplayFullText());
+		}
+	}
+}
