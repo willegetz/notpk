17595f33d12ba1def40dc5f3ce306ab75defe3fe
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-01-04 17:16:08 +0000

    More migration to DisplayUtilities

--- ItemSmithWorkShop/ItemSmithWorkShop/WeaponUtilities/DisplayUtilities.cs
+++ ItemSmithWorkShop/ItemSmithWorkShop/WeaponUtilities/DisplayUtilities.cs
@@ -8,26 +8,28 @@ namespace ItemSmithWorkShop.WeaponUtilities
 {
 	public class DisplayUtilities : WeaponItemWeaver
 	{
-		public static string FullMagicalDisplay(MagicWeaponItem magicWeapon)
+		public static string FullMagicalDisplay(WeaponItemWeaver magicWeapon)
 		{
 			var sb = new StringBuilder();
-			sb.AppendLine(string.Format("{0}:\t'{1} gp'\t('+{10}')\r\nTo Hit: '+{2}'\r\nDamage: '{3}{4}' {5}\r\nHardness: '{6}'\r\nHit Points: '{7}'\r\nWeight: '{8} pound(s)'\r\n\t{9}",
+			sb.AppendLine(string.Format("{0}:\t'{1} gp'\t('+{11} value')\r\nTo Hit: '+{2}'\r\nDamage: '{3}{4}{5}' {6}\r\nHardness: '{7}'\r\nHit Points: '{8}'\r\nWeight: '{9} pound(s)'\r\n\t{10}",
 				magicWeapon.GetName(),
 				magicWeapon.GetCost(),
 				magicWeapon.GetEnhancementBonus(),
 				magicWeapon.GetDamage(),
 				magicWeapon.GetThreat(),
+				magicWeapon.GetEnchantmentCriticalDamage(),
 				magicWeapon.GetDamageType(),
 				magicWeapon.GetModifiedHardness(),
 				magicWeapon.GetModifiedHitPoints(),
 				magicWeapon.GetWeight(),
 				magicWeapon.GetDescription(),
 				magicWeapon.GetEnhancementBonusForCost()));
-			sb.AppendLine(string.Format("\r\nCreator Caster Level: '{0}'\r\nTime to Create: '{1} Days'\r\nCreation XP Cost: '{2}'\r\nCreation Raw Material Cost: '{3}'",
+			sb.AppendLine(string.Format("\r\nCreator Caster Level: '{0}'\r\nTime to Create: '{1} Days'\r\nCreation XP Cost: '{2}'\r\nCreation Raw Material Cost: '{3}'\r\n{4}",
 				magicWeapon.GetMinimumCasterLevel(),
 				magicWeapon.GetDaysToCreate(),
 				magicWeapon.GetCreationXpCost(),
-				magicWeapon.GetCreationRawMaterialCost()));
+				magicWeapon.GetCreationRawMaterialCost(),
+				magicWeapon.GetCreationRequirements()));
 
 			return sb.ToString();
 		}
--- ItemSmithWorkShop/ItemSmithWorkShop/WeaponUtilities/EnchantmentDictionary.cs
+++ ItemSmithWorkShop/ItemSmithWorkShop/WeaponUtilities/EnchantmentDictionary.cs
@@ -8,7 +8,6 @@ namespace ItemSmithWorkShop.WeaponUtilities
 {
 	public class EnchantmentDictionary : AdventureItem
 	{
-		private const string requiredFeats = "Craft Magic Arms and Armor";
 		private static Dictionary<string, EnchantmentData> weaponEnchantments;
 
 		static EnchantmentDictionary()
@@ -29,7 +28,7 @@ namespace ItemSmithWorkShop.WeaponUtilities
 					"Flaming", new EnchantmentData
 					{
 						EnchantmentName = "Flaming", EnchantmentDamage = " +1D6", MinimumCasterLevel = 10, EnhancementBonus = 1,
-						DamageType = "Fire", CreationRequirements = string.Format("\r\n\tRequired Feats: {0}\r\n\tRequired Spells: flame blade, flame strike, or fireball", requiredFeats),
+						DamageType = "Fire", CreationRequirements = string.Format("\r\n\tRequired Spells: flame blade, flame strike, or fireball"),
 						EnchantmentDescription = "\n\n\tUpon command, a flaming weapon is sheathed in fire.\r\n\tThe fire does not harm the wielder. The effect\r\n\tremains until another command is given."
 					}
 				},
@@ -37,14 +36,14 @@ namespace ItemSmithWorkShop.WeaponUtilities
 					"Icy Burst", new EnchantmentData
 					{
 						EnchantmentName = "Icy Burst", EnchantmentCriticalDamage = " +1d10", MinimumCasterLevel = 10, EnhancementBonus = 2,
-						DamageType = "Cold", CreationRequirements = string.Format("\r\n\tRequired Feats: {0}\r\n\tRequired Spells: chill metal or ice storm", requiredFeats),
+						DamageType = "Cold", CreationRequirements = string.Format("\r\n\tRequired Spells: chill metal or ice storm"),
 						EnchantmentDescription = "\n\n\tAn icy burst weapon functions as a frost weapon that also\r\n\texplodes with frost upon striking a successful critical hit.\r\n\tThe frost does not harm the wielder. In addition to the extra\r\n\tdamage from the frost ability, an icy burst weapon deals an\r\n\textra 1d10 points of cold damage on a successful critical hit.\r\n\tIf the weapon’s critical multiplier is ×3, add an extra 2d10\r\n\tpoints of cold damage instead, and if the multiplier is ×4,\r\n\tadd an extra 3d10 points. Bows, crossbows, and slings so\r\n\tcrafted bestow the cold energy upon their ammunition.\r\n\tEven if the frost ability is not active, the weapon still\r\n\tdeals its extra cold damage on a successful critical hit."
 					}
 				},
 				{
 					"Vorpal", new EnchantmentData 
 					{
-						EnchantmentName = "Vorpal", MinimumCasterLevel = 18, EnhancementBonus = 5, CreationRequirements = string.Format("\r\n\tRequired Feats: {0}\r\n\tRequired Spells: circle of death, keen edge", requiredFeats),
+						EnchantmentName = "Vorpal", MinimumCasterLevel = 18, EnhancementBonus = 5, CreationRequirements = string.Format("\r\n\tRequired Spells: circle of death, keen edge"),
 						EnchantmentDescription = "\n\n\tThis potent and feared ability allows the weapon to sever the heads of\r\n\tthose it strikes. Upon a roll of natural 20 (followed by a successful\r\n\troll to confirm the critical hit), the weapon severs the opponent's\r\n\thead (if it has one) from its body. Some creatures, such as many\r\n\taberrations and all oozes, have no heads. Others, such as golems\r\n\tand undead creatures other than vampires, are not affected by the\r\n\tloss of their heads. Most other creatures, however, die when their\r\n\theads are cut off.\r\n\tA vorpal weapon must be a slashing weapon."
 					}
 				},
