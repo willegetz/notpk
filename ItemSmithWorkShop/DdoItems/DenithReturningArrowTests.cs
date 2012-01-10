266684b0227e2b759dbba381bc3d9b5cba1e95ac
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-01-10 17:03:16 +0000

    House Denith returning arrows calculation for Dungeons and Dragons Online added

---
+++ ItemSmithWorkShop/DdoItems/DenithReturningArrowTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace DdoItems
+{
+	[TestClass]
+	[UseReporter(typeof(DiffReporter))]
+	public class DenithReturningArrowTests
+	{
+		[TestMethod]
+		public void TestDenithBolts()
+		{
+			var quantity = DenithBolts(1500);
+			Approvals.Approve(quantity);
+		}
+
+		private string DenithBolts(double quantity)
+		{
+			var remainingBolts = new StringBuilder();
+			var tmp = quantity;
+			remainingBolts.AppendLine(quantity.ToString() + "\t\t" + tmp.ToString());
+
+			var lastBolt = 1;
+
+			do
+			{
+				quantity = Math.Floor(quantity * 0.75);
+				tmp += quantity;
+				remainingBolts.AppendLine(quantity.ToString() + "\t\t" + tmp.ToString());
+
+			} while (quantity > lastBolt);
+
+			return remainingBolts.ToString();
+		}
+	}
+}
