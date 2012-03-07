791aa19d077699cdaea91f7e6fb6bd27744d255f
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-03-07 07:39:45 +0000

    Updated NuGet at base terminal. Weapon enchantment class made.

--- trunk/ItemSmithWorkShop/DdoItems/Properties/AssemblyInfo.cs
+++ trunk/ItemSmithWorkShop/DdoItems/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using ApprovalTests.Reporters;
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-//[assembly: UseReporter(typeof(TortoiseDiffReporter))]
+[assembly: UseReporter(typeof(DiffReporter))]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
