de62fac29c4b45f2f586048a66229c9f85a78b95
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-03-08 00:33:17 +0000

    Spike on simple manipulation.

--- trunk/ItemSmithWorkShop/ItemSmithWorkShop/Tests/NewWeaponTests/PhbWeaponTests.cs
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/Tests/NewWeaponTests/PhbWeaponTests.cs
@@ -1,4 +1,5 @@
-﻿using ApprovalTests;
+﻿using System.Text;
+using ApprovalTests;
 using ItemSmithWorkShop.Weapons;
 using ItemSmithWorkShop.Weapons.MaterialTypes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -28,6 +29,33 @@ namespace ItemSmithWorkShop.Tests.NewWeaponTests
 		{
 			var anarchic = new WeaponEnchantment();
 			Approvals.Verify(anarchic.ToString());
+		}
+
+		[TestMethod]
+		public void TestBlah()
+		{
+			var dagger = new PhbWeapon();
+			var mithral = new Mithral();
+			var anarchic = new WeaponEnchantment();
+
+			var name = new StringBuilder();
+
+			if (anarchic.Affix == "Pre")
+			{
+				name.Append(string.Format("{0} {1} {2}", anarchic.EnchantmentName, mithral.MaterialName, dagger.WeaponName));
+			}
+			else
+			{
+				name.Append(string.Format("{0} {1} of {2}", dagger.WeaponName, mithral.MaterialName, anarchic.EnchantmentName));
+			}
+
+			var sb = new StringBuilder();
+
+			sb.AppendLine(string.Format("Name: {1} {2} {0}", anarchic.EnchantmentName, mithral.MaterialName, dagger.WeaponName));
+			sb.AppendLine();
+			sb.AppendLine(name.ToString());
+
+			Approvals.Verify(sb.ToString());
 		}
 	}
 }
