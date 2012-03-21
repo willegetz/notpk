e7a95c2c9cefb6f9d77d1343641203c69bc7b25a
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-03-21 06:07:28 +0000

    Some cleanup. Added masterwork alchemical silver component.

--- ItemSmithWorkShop/ItemSmithWorkShop/Items/MaterialTypes/MasterworkAlchemicalSilver.cs
+++ ItemSmithWorkShop/ItemSmithWorkShop/Items/MaterialTypes/MasterworkAlchemicalSilver.cs
@@ -6,52 +6,30 @@ using ItemSmithWorkShop.Items.Interfaces;
 
 namespace ItemSmithWorkShop.Items.MaterialTypes
 {
-	public class AlchemicalSilver : IMaterialComponent
+	public class MasterworkAlchemicalSilver : IMaterialComponent
 	{
-		// Alchemical siver can be applied to:
-		//		Ammunition: +2 gold
-		//		Light weapons: +20 gold
-		//		One Handed weapons, or one head of a double weapon: +90 gold
-		//		Two-handed weapons, or both heads of a double weapon: +180 gold
-		// Weapons always receive a -1 to damage roll
-		// Alchemical silver does not work on rare materials
-
 		private const double ammunitionCostModifier = 2;
 		private const double lightWeaponCostModifier = 20;
 		private const double oneHandedWeaponCostModifier = 90;
 		private const double twoHandedWeaponCostModifier = 180;
+		private const double masterworkCostModifier = 300;
 
-		public string ComponentName
-		{
-			get { return "Alchemical Silver"; ; }
-		}
+		public string ComponentName { get { return "Masterwork Alchemical Silver"; } }
 
-		// The multiple cost modifiers based on the kind of weapon calls into question the need for this property.
-		public double WeaponCostModifier
-		{
-			get { return 0; }
-		}
+		// property returns '0' as the there are four different modifiers that can be applied depending on circumstance.
+		public double WeaponCostModifier { get { return 0; } }
 
-		public bool IsMasterwork
-		{
-			get { return false; }
-		}
+		public bool IsMasterwork { get { return true; } }
 
-		public string ToHitBonus
-		{
-			get { return string.Empty; }
-		}
+		public string ToHitBonus { get { return "+1"; } }
 
-		public string ComponentInfo
-		{
-			get { return "I am an alchemical silver weapon"; }
-		}
+		public string ComponentInfo { get { return "I am a masterwork alchemical silver weapon"; } }
 
 		public double ApplyCostModifier(IWeapon weapon)
 		{
 			if (weapon.WeaponCategory.Contains("Light"))
 			{
-				return weapon.WeaponCost + lightWeaponCostModifier;
+				return weapon.WeaponCost + lightWeaponCostModifier + masterworkCostModifier;
 			}
 			return weapon.WeaponCost;
 		}
