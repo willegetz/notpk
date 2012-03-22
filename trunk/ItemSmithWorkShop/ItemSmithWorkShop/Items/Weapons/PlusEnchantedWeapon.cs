910943365af761e4af96221f552c70ca5c175691
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-03-22 22:34:37 +0000

    Added notes and expanded stubs for plus enchanted weapon.

--- trunk/ItemSmithWorkShop/ItemSmithWorkShop/Items/Weapons/PlusEnchantedWeapon.cs
+++ trunk/ItemSmithWorkShop/ItemSmithWorkShop/Items/Weapons/PlusEnchantedWeapon.cs
@@ -9,16 +9,27 @@ namespace ItemSmithWorkShop.Items.Weapons
 {
 	public class PlusEnchantedWeapon : IWeapon
 	{
+		// In addition to the weapon and forged weapon properties A PlusEnchantedWeapon has
+		//		A Plus to attack and damage
+		//		A cost to create based on the plus of the enchantment
+		//		The plus modifier goes at the beginning of the name
+		//		A change to hardness and hit points
+		//		A magic aura
+		//		A chance that it glows
 
-		public PlusEnchantedWeapon(IWeapon forgedWeapon)
-		{
-			if (!(forgedWeapon is ForgedWeapon))
-			{
-				forgedWeapon = new ForgedWeapon(forgedWeapon, new Masterwork());
-			}
-		}
+		//	For Creation purposes:
+		//		A minimum caster level
+		//		Time to create
+		//		Gold and Xp cost to create in addition to the plus cost
+		//		A raw material cost 
+		//		Choice to make it glow
+		//		Feats required
+		
+		ForgedWeapon forgedWeapon;
 
-		public string GivenName { get { throw new NotImplementedException(); } }
+		public string ComponentName { get; private set; }
+
+		public string GivenName { get; private set; }
 
 		public string Proficiency { get { throw new NotImplementedException(); } }
 
@@ -30,7 +41,7 @@ namespace ItemSmithWorkShop.Items.Weapons
 
 		public string WeaponSize { get { throw new NotImplementedException(); } }
 
-		public string WeaponName { get { throw new NotImplementedException(); } }
+		public string WeaponName { get; private set; }
 
 		public double WeaponCost { get { throw new NotImplementedException(); } }
 
@@ -53,5 +64,54 @@ namespace ItemSmithWorkShop.Items.Weapons
 		public double RangeIncrement { get { throw new NotImplementedException(); } }
 
 		public double MaxRange { get { throw new NotImplementedException(); } }
+
+		public PlusEnchantedWeapon(IWeapon weapon, int plusEnhancement)
+		{
+			forgedWeapon = QualifyWeapon(weapon);
+
+			WeaponName = TrimComponentName(forgedWeapon);
+		}
+
+
+		private ForgedWeapon QualifyWeapon(IWeapon weapon)
+		{
+			if (!(weapon is ForgedWeapon))
+			{
+				return new ForgedWeapon(weapon, new Masterwork());
+			}
+			else if ((weapon is ForgedWeapon) && !weapon.IsMasterwork)
+			{
+				return MasterworkVersionOfComponent(weapon);
+			}
+			else
+			{
+				return weapon as ForgedWeapon;
+			}
+		}
+
+		private ForgedWeapon MasterworkVersionOfComponent(ForgedWeapon weapon)
+		{
+			if (weapon.ComponentName = "Cold Iron")
+			{
+				return new ForgedWeapon(weapon, new MasterworkColdIron());
+			}
+			else
+			{
+				return new ForgedWeapon(weapon, new MasterworkAlchemicalSilver());
+			}
+		}
+
+		private string TrimComponentName(ForgedWeapon weapon)
+		{
+			string masterwork = "Masterwork ";
+			if (weapon.ComponentName.Contains("Masterwork"))
+			{
+				return weapon.WeaponName.Remove(0, masterwork.Length);
+			}
+			else
+			{
+				return weapon.WeaponName;
+			}
+		}
 	}
 }
