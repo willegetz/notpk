867c2aad14bbdfb997c0c410d7c1b8e3526ca2d9
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-06-10 23:09:27 +0000

    [Apothem] Updated math functions so that the input value for a given parameter will be appropriately reflected in the output table.

--- HexagonWebsiteDocumentReady.js
+++ HexagonWebsiteDocumentReady.js
@@ -1,6 +1,4 @@
-﻿$(document).ready(function () {
-
-	//$("#gpWidget-1").hide();
+﻿	$(document).ready(function () {
 
 	hideAll();
 
@@ -11,40 +9,75 @@
 		switch (calcType) {
 			case "selectApothem":
 				derivedSideLength = sideLengthApothem(inputValue);
+				$("#apothem").text(inputValue);
+				$("#area").text(areaFromSideLength(derivedSideLength));
+				$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
+				$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
+				$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
+				$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
+				$("#side").text(sideLengthFromSideLength(derivedSideLength));
 				break;
 			case "selectArea":
 				derivedSideLength = sideLengthArea(inputValue);
+				$("#apothem").text(apothemLength(derivedSideLength));
+				$("#area").text(inputValue);
+				$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
+				$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
+				$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
+				$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
+				$("#side").text(sideLengthFromSideLength(derivedSideLength));
 				break;
 			case "selectCenterToPoint":
 				derivedSideLength = sideLengthCtoP(inputValue);
+				$("#apothem").text(apothemLength(derivedSideLength));
+				$("#area").text(areaFromSideLength(derivedSideLength));
+				$("#centertoPoint").text(inputValue);
+				$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
+				$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
+				$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
+				$("#side").text(sideLengthFromSideLength(derivedSideLength));
 				break;
 			case "selectFlatToFlat":
 				derivedSideLength = sideLengthFtoF(inputValue);
+				$("#apothem").text(apothemLength(derivedSideLength));
+				$("#area").text(areaFromSideLength(derivedSideLength));
+				$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
+				$("#flatToFlat").text(inputValue);
+				$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
+				$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
+				$("#side").text(sideLengthFromSideLength(derivedSideLength));
 				break;
 			case "selectPerimeter":
 				derivedSideLength = sideLengthPerimeter(inputValue);
+				$("#apothem").text(apothemLength(derivedSideLength));
+				$("#area").text(areaFromSideLength(derivedSideLength));
+				$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
+				$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
+				$("#perimeter").text(inputValue);
+				$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
+				$("#side").text(sideLengthFromSideLength(derivedSideLength));
 				break;
 			case "selectPointToPoint":
 				derivedSideLength = sideLengthPtoP(inputValue);
+				$("#apothem").text(apothemLength(derivedSideLength));
+				$("#area").text(areaFromSideLength(derivedSideLength));
+				$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
+				$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
+				$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
+				$("#pointToPoint").text(inputValue);
+				$("#side").text(sideLengthFromSideLength(derivedSideLength));
 				break;
 			case "selectSide":
 				derivedSideLength = sideLength(inputValue);
+				$("#apothem").text(apothemLength(derivedSideLength));
+				$("#area").text(areaFromSideLength(derivedSideLength));
+				$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
+				$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
+				$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
+				$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
+				$("#side").text(inputValue);
 				break;
 		}
-
-		$("#apothem").text(apothemLength(derivedSideLength));
-
-		$("#area").text(areaFromSideLength(derivedSideLength));
-
-		$("#centertoPoint").text(centerToVertexFromSideLength(derivedSideLength));
-
-		$("#flatToFlat").text(sideToSideFromSideLength(derivedSideLength));
-
-		$("#perimeter").text(perimeterFromSideLength(derivedSideLength));
-
-		$("#pointToPoint").text(vertexToVertexFromSide(derivedSideLength));
-
-		$("#side").text(sideLengthFromSideLength(derivedSideLength));
 	});
 
 	$("#givenParameter").change(function () {
