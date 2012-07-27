0cb7c248568434d98666b313844b0ece16b53416
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-07-27 07:19:15 +0000

    [CS211]  Beginning work on Lab03.

---
+++ trunk/Learning/CS211/Lab03/Lab03/Lab03/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab03
+{
+	public class Course
+	{
+		public long CourseNum;
+		public string CourseName;
+	}
+}
---
+++ trunk/Learning/CS211/Lab03/Lab03/Lab03/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab03
+{
+	public class Person
+	{
+		public long SSN;
+		public string Fname;
+		public string Lname;
+		public int Age;
+		public char Gender; // M for male, F for female
+	}
+}
