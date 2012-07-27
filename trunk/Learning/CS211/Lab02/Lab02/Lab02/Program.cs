71c2225d512c0ba8af581da760ef101afd223330
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-07-27 06:21:33 +0000

    [CS211]  Completed lab 2.

---
+++ trunk/Learning/CS211/Lab02/Lab02/Lab02/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lab02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var V = new List<string>();
+
+			BuildBeginningVector(V);
+
+			var fileStream = new FileStream(@"C:\Users\William\Documents\Visual Studio 2010\NoTPK\trunk\Learning\CS211\Lab02\Lab02\Files\data.txt", FileMode.Open, FileAccess.Read);
+			using (var file = new StreamReader(fileStream))
+			{
+				PerformTransaction(V, file);
+				
+			}
+
+		}
+		private static void BuildBeginningVector(List<string> V)
+		{
+			V.Add("In");
+			V.Add("a");
+			V.Add("hole");
+			V.Add("in");
+			V.Add("the");
+			V.Add("ground");
+			V.Add("there");
+			V.Add("lived");
+			V.Add("a");
+			V.Add("hobbit.");
+		}
+
+		private static void PerformTransaction(List<string> v, StreamReader file)
+		{
+			string [] operationArray = new string [v.Count];
+
+			while (!file.EndOfStream)
+			{
+				operationArray = file.ReadLine().ToString().Split('\t');
+				ExecuteTransaction(v, operationArray);
+			}
+			var blah = operationArray.Length;
+		}
+
+		private static void ExecuteTransaction(List<string> v, string[] operationArray)
+		{
+			switch (operationArray[0])
+			{
+				case "Insert":
+					PerformInsert(v, operationArray);
+					break;
+				case "Delete":
+					PerformDelete(v, operationArray);
+					break;
+				case "Print":
+					PerformPrint(v);
+					break;
+				default:
+					break;
+			}
+		}
+		private static void PerformInsert(List<string> v, string[] operationArray)
+		{
+			int insertIndex = int.Parse(operationArray[2]);
+			if (insertIndex <= v.Count)
+			{
+				v.Insert(insertIndex, operationArray[1]);
+			}
+			else
+			{
+				v.Add(string.Format("Cannot insert: '{0}' at Index '{1}'\r\nThe list is '{2}' long", operationArray[1], insertIndex, v.Count));
+			}
+		}
+
+		private static void PerformDelete(List<string> v, string[] operationArray)
+		{
+			int deleteIndex = int.Parse(operationArray[2]);
+			if (deleteIndex <= v.Count)
+			{
+				v.RemoveAt(deleteIndex);
+			}
+			else
+			{
+				v.Add(string.Format("Cannot delete at Index '{0}'\r\nThe list is '{2}' long", deleteIndex, v.Count));
+			}
+		}
+
+		private static void PerformPrint(List<string> v)
+		{
+			var sb = new StringBuilder();
+			foreach (var item in v)
+			{
+				sb.Append(item + " ");
+			}
+			Console.WriteLine(sb.ToString());
+			Console.ReadKey();
+		}
+	}
+}
