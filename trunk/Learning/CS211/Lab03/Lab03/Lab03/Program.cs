4dc3ca8cf5563112d0218b4b55a9820b8b616790
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-07-27 19:02:15 +0000

    [Lab03]  Variables in place for holding contents of filestream.

--- trunk/Learning/CS211/Lab03/Lab03/Lab03/Program.cs
+++ trunk/Learning/CS211/Lab03/Lab03/Lab03/Program.cs
@@ -11,7 +11,7 @@ namespace Lab03
 		static void Main(string[] args)
 		{
 			List<StudentProfile> StCollection = new List<StudentProfile>();
-			var fileStream = new FileStream(@"C:\Users\William\Documents\Visual Studio 2010\NoTPK\trunk\Learning\CS211\Lab03\Lab03\Files\data.txt", FileMode.Open, FileAccess.Read);
+			var fileStream = new FileStream(@"C:\Documents and Settings\wgetz\Learning\CS211\Lab03\Lab03\Files\data.txt", FileMode.Open, FileAccess.Read);
 			using (var file = new StreamReader(fileStream))
 			{
 				while (!file.EndOfStream)
@@ -23,7 +23,29 @@ namespace Lab03
 
 		private static void AddStudent(List<StudentProfile> stCollection, StreamReader file)
 		{
+			// Apparently, readline will consume the first line of the file.
+			// Subsequent readlines after the first will take the next line after, etc.
+			// This continues whilst the stream is open.
+			// var blah = file.ReadLine();
+//			var blah1 = file.ReadToEnd().Split('\r');
+
+//			var name = file.ReadLine().Split(' ');
+//			var age = file.ReadLine();
+//			var sex = file.ReadLine();
+//			var ssn = file.ReadLine().Split('-');
+//			var studentNumber = file.ReadLine();
+//			var course1 = file.ReadLine().Split(' ');
+//			var course2 = file.ReadLine().Split(' ');
+//			var course3 = file.ReadLine().Split(' ');
+
+			var stuffs = file.ReadToEnd().Split(',');
+
 			var studentProfile = new StudentProfile();
+			string[] blah;
+			foreach (var stuff in stuffs)
+			{
+				blah = stuff.ToString().Split("\r\n");
+			}
 		}
 	}
 }
