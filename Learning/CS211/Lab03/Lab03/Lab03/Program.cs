b9983cb13782a8d84f0b950c78ee7f345a1ca0ab
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-07-27 07:19:15 +0000

    [CS211]  Beginning work on Lab03.

---
+++ Learning/CS211/Lab03/Lab03/Lab03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lab03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<StudentProfile> StCollection = new List<StudentProfile>();
+			var fileStream = new FileStream(@"C:\Users\William\Documents\Visual Studio 2010\NoTPK\trunk\Learning\CS211\Lab03\Lab03\Files\data.txt", FileMode.Open, FileAccess.Read);
+			using (var file = new StreamReader(fileStream))
+			{
+				while (!file.EndOfStream)
+				{
+					AddStudent(StCollection, file);
+				}	
+			}
+		}
+
+		private static void AddStudent(List<StudentProfile> stCollection, StreamReader file)
+		{
+			var studentProfile = new StudentProfile();
+		}
+	}
+}
---
+++ Learning/CS211/Lab03/Lab03/Lab03/StudentProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab03
+{
+	public class StudentProfile
+	{
+		public Person PersonalInfo;
+		public Student StInfo;
+	}
+}
