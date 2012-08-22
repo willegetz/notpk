08284a74c970f4681a4a1eacf7cc64cf62190731
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-08-22 06:29:08 +0000

    [TrapArchitect]  Started work on a trap effect factory.

---
+++ branches/TrapArchitect/DungeonBuildersGuidebook1/Factories/SpecificTrapFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonBuildersGuidebook1.Factories
+{
+	public class SpecificTrapFactory
+	{
+		private string effectDescription;
+
+		public SpecificTrapFactory(string effectDescription)
+		{
+			// TODO: Complete member initialization
+			this.effectDescription = effectDescription;
+		}
+
+		public string Get()
+		{
+			return effectDescription.ToLower();
+		}
+	}
+}
--- branches/TrapArchitect/DungeonBuildersGuidebook1/TrapComponentObjects/TrapEffects.cs
+++ branches/TrapArchitect/DungeonBuildersGuidebook1/TrapComponentObjects/TrapEffects.cs
@@ -2,13 +2,13 @@
 {
 	public class TrapEffects
 	{
-		public bool RollAgain { get; set; }
-		public bool HasSubtable { get; set; }
-		public int RollUpperBound { get; set; }
+		public bool RollAgain { get; set; } 
+		public bool HasSubtable { get; set; } // Subtable
+		public int RollUpperBound { get; set; }  // Setting the top of the range in the dictionary
 		public int NumberOfReRolls { get; set; }
 		public string EffectDescription { get; set; }
 		public string AdditionalInformation { get; set; }
-		public string SubtableName { get; set; }
-		public string SubtableEffectDescription { get; set; }
+		public string SubtableName { get; set; } // Subtable
+		public string SubtableEffectDescription { get; set; } // Subtable
 	}
 }
