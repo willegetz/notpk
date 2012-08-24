65af69bcf31a5de58b8c2947c9d77ccaa5cead50
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-08-24 06:37:33 +0000

    [TrapArchitect]  Removed old code.  Some renaming.

--- DungeonBuildersGuidebook1/TrapLoaders/DataConstants.cs
+++ DungeonBuildersGuidebook1/TrapLoaders/DataConstants.cs
@@ -5,14 +5,14 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace DungeonBuildersGuidebook1.TrapComponentLogic
+namespace DungeonBuildersGuidebook1.TrapLoaders
 {
 	public static class DataConstants
 	{
 		public static string DataFilesPath
 		{
-//			get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\DataFiles\"; }
-			get { return @"DataFiles\"; }
+			get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\DataFiles\"; }
+			//get { return @"DataFiles\"; }
 		}
 	}
 }
