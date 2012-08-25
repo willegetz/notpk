a9cfe59587455dd28b66d680a5719786d081a618
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-08-25 00:22:42 +0000

    [TrapArchitect]  Added tests for subtables.

--- DungeonBuildersGuidebook1/TrapLoaders/TableLoader.cs
+++ DungeonBuildersGuidebook1/TrapLoaders/TableLoader.cs
@@ -11,11 +11,15 @@ namespace DungeonBuildersGuidebook1.TrapLoaders
 	public class TableLoader
 	{
 		private string xmlTrapSubtablesFilePath = DataConstants.DataFilesPath + "TrapEffectsSubtables.xml";
+		private string xmlTrapDamagesFilePath = DataConstants.DataFilesPath + "TrapDamages.xml";
+
 		private XElement trapSubtablesXml;
+		private XElement trapDamageXml;
 
 		public TableLoader()
 		{
 			trapSubtablesXml = XElement.Load(xmlTrapSubtablesFilePath);
+			trapDamageXml = XElement.Load(xmlTrapDamagesFilePath);
 		}
 
 		public DiceCup GetPitTrapTableDie()
@@ -33,6 +37,26 @@ namespace DungeonBuildersGuidebook1.TrapLoaders
 																	).OrderBy(r => r.RollUpperBound);
 		}
 
+
+		public DiceCup GetGasTrapTableDie()
+		{
+			return new DiceCup(DiceDefinition.Parse(trapSubtablesXml.Descendants("GasTrap").Select(d => d.Element("TableDieRoll").Element("DiceDefinition").Value).Single()));
+		}
+
+		public IEnumerable<GasTypes> GetGasTrpContents()
+		{
+			return trapSubtablesXml.Descendants("GasType").Select(gT => new GasTypes()
+																	{
+																		RollUpperBound = int.Parse(gT.Element("RollUpperBound").Value),
+																		GasName = gT.Element("GasName").Value,
+																	}
+																).OrderBy(r => r.RollUpperBound);
+		}
+
+		public DiceCup GetTrapDamageTableDie()
+		{
+			return new DiceCup(DiceDefinition.Parse(trapDamageXml.Descendants("TableDieRoll").Select(d => d.Element("DiceDefinition").Value).Single()));
+		}
 	}
 
 }
