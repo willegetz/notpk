a7489e40a0a86f583a1fe28cb8904ea337f3f9de
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-08-25 05:17:02 +0000

    [TrapArchitect]  Tables now loaded from a separate class.

--- DungeonBuildersGuidebook1/TrapLoaders/TableLoader.cs
+++ DungeonBuildersGuidebook1/TrapLoaders/TableLoader.cs
@@ -10,24 +10,76 @@ namespace DungeonBuildersGuidebook1.TrapLoaders
 {
 	public class TableLoader
 	{
-		private string xmlTrapSubtablesFilePath = DataConstants.DataFilesPath + "TrapEffectsSubtables.xml";
-		private string xmlTrapDamagesFilePath = DataConstants.DataFilesPath + "TrapDamages.xml";
+		private readonly string xmlTrapComponentsFilePath = DataConstants.DataFilesPath + "TrapComponents.xml";
+		private readonly string xmlTrapEffectsFilePath = DataConstants.DataFilesPath + "TrapEffectsAndTraits.xml";
+		private readonly string xmlTrapSubtablesFilePath = DataConstants.DataFilesPath + "TrapEffectsSubtables.xml";
+		private readonly string xmlTrapDamagesFilePath = DataConstants.DataFilesPath + "TrapDamages.xml";
 
-		private XElement trapSubtablesXml;
-		private XElement trapDamageXml;
+		private static XElement trapBasesXml;
+		private static XElement trapEffectsXml;
+		private static XElement trapSubtablesXml;
+		private static XElement trapDamageXml;
 
 		public TableLoader()
 		{
+			trapBasesXml = XElement.Load(xmlTrapComponentsFilePath);
+			trapEffectsXml = XElement.Load(xmlTrapEffectsFilePath);
 			trapSubtablesXml = XElement.Load(xmlTrapSubtablesFilePath);
 			trapDamageXml = XElement.Load(xmlTrapDamagesFilePath);
 		}
 
-		public DiceCup GetPitTrapTableDie()
+		public static DiceCup GetTrapBaseTableDie()
+		{
+			return new DiceCup(DiceDefinition.Parse(trapBasesXml.Descendants("TableDieRoll").Select(d => d.Element("DiceDefinition").Value).Single()));
+		}
+
+		public static IEnumerable<TrapBases> GetTrapBaseContents()
+		{
+			return trapBasesXml.Descendants("TrapBase").Select(tB => new TrapBases()
+																{
+																	RollUpperBound = int.Parse(tB.Element("RollUpperBound").Value),
+																	TrappedObjectOrArea = tB.Element("TrappedObjectOrArea").Value,
+																	MechanismTypeSpecified = bool.Parse(tB.Element("MechanismTypeSpecified").Value)
+																}
+															).OrderBy(r => r.RollUpperBound);
+		}
+
+		public static DiceCup GetTrapMechanismTableDie()
+		{
+		   return new DiceCup(DiceDefinition.Parse(trapBasesXml.Descendants("MechanismDieRoll").Select(d => d.Element("DiceDefinition").Value).Single()));
+		}
+
+		public static List<KeyValuePair<int, string>> GetTrapMechanismContents()
+		{
+		  return trapBasesXml.Descendants("Mechanism").ToDictionary(m => int.Parse(m.Element("RollUpperBound").Value), m => m.Element("MechanismType").Value).ToList();
+		}
+
+		public static DiceCup GetTrapEffectsTableDie()
+		{
+			return new DiceCup(DiceDefinition.Parse(trapEffectsXml.Descendants("TableDieRoll").Select(d => d.Element("DiceDefinition").Value).Single()));
+		}
+
+		public static IEnumerable<TrapEffects> GetTrapEffectsContents()
+		{
+			return trapEffectsXml.Descendants("Effect").Select(tE => new TrapEffects()
+																{
+																	RollUpperBound = int.Parse(tE.Element("RollUpperBound").Value),
+																	EffectDescription = tE.Element("EffectDescription").Value,
+																	AdditionalInformation = ValidateElement(tE.Element("AdditionalInformation").Value),
+																	RollAgain = bool.Parse(tE.Element("RollAgain").Value),
+																	NumberOfReRolls = ValidateIntElement(tE.Element("NumberOfReRolls").Value),
+																	HasSubtable = bool.Parse(tE.Element("HasSubtable").Value),
+																	SubtableName = tE.Element("SubtableName").Value,
+																}
+															).OrderBy(r => r.RollUpperBound);
+		}
+
+		public static DiceCup GetPitTrapTableDie()
 		{
 			return new DiceCup(DiceDefinition.Parse(trapSubtablesXml.Descendants("PitContents").Select(d => d.Element("TableDieRoll").Element("DiceDefinition").Value).Single()));
 		}
 
-		public IEnumerable<PitContents> GetPitTrapContents()
+		public static IEnumerable<PitContents> GetPitTrapContents()
 		{
 			return trapSubtablesXml.Descendants("ContentType").Select(pC => new PitContents()
 																		{
@@ -38,12 +90,12 @@ namespace DungeonBuildersGuidebook1.TrapLoaders
 		}
 
 
-		public DiceCup GetGasTrapTableDie()
+		public static DiceCup GetGasTrapTableDie()
 		{
 			return new DiceCup(DiceDefinition.Parse(trapSubtablesXml.Descendants("GasTrap").Select(d => d.Element("TableDieRoll").Element("DiceDefinition").Value).Single()));
 		}
 
-		public IEnumerable<GasTypes> GetGasTrpContents()
+		public static IEnumerable<GasTypes> GetGasTrpContents()
 		{
 			return trapSubtablesXml.Descendants("GasType").Select(gT => new GasTypes()
 																	{
@@ -53,9 +105,44 @@ namespace DungeonBuildersGuidebook1.TrapLoaders
 																).OrderBy(r => r.RollUpperBound);
 		}
 
-		public DiceCup GetTrapDamageTableDie()
+		public static DiceCup GetTrapDamageTableDie()
 		{
 			return new DiceCup(DiceDefinition.Parse(trapDamageXml.Descendants("TableDieRoll").Select(d => d.Element("DiceDefinition").Value).Single()));
+		}
+
+		public static IEnumerable<TrapDamages> GetTrapDamages()
+		{
+			return trapDamageXml.Descendants("Damage").Select(tD => new TrapDamages()
+																	{
+																		RollUpperBound = int.Parse(tD.Element("RollUpperBound").Value),
+																		DamageDescription = tD.Element("DamageDescription").Value,
+																	}
+																).OrderBy(r => r.RollUpperBound);
+		}
+
+
+		private static int ValidateIntElement(string intElementValue)
+		{
+			if (string.IsNullOrEmpty(intElementValue))
+			{
+				return 0;
+			}
+			else
+			{
+				return int.Parse(intElementValue);
+			}
+		}
+
+		private static string ValidateElement(string elementValue)
+		{
+			if (string.IsNullOrEmpty(elementValue))
+			{
+				return string.Empty;
+			}
+			else
+			{
+				return elementValue;
+			}
 		}
 	}
 
