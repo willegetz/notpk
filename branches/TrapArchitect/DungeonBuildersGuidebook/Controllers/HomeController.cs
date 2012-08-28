cbe5096a8bc86b5f96df1394e77fe9891730b401
WillEGetz <WillEGetz@5692b0c4-607f-4d96-ac28-de0cdc1f6f93>
2012-08-28 20:51:44 +0000

    [TrapArchitect]  So begins my mvc app...

---
+++ branches/TrapArchitect/DungeonBuildersGuidebook/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DungeonBuildersGuidebook.Models;
+
+namespace DungeonBuildersGuidebook.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+
+		public ActionResult TrapArchitect()
+		{
+			return View(new TrapArchitectModel());
+		}
+	}
+}
